5a8c174ff721a4944e092ebfd71fa339e71eeb93
Andrew Tursi <atursi@gmail.com>
2026-03-10 18:13:19 +0000

    fix: move admin URL to env, restore BlogSitemap, track urls.py

--- core/sitemaps.py
+++ core/sitemaps.py
@@ -1,26 +1,27 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
-from core.models import Course  # <--- Crucial: Import your database model
+from core.models import Course, BlogPost
 
 class StaticViewSitemap(Sitemap):
     priority = 0.8
     changefreq = 'weekly'
-
     def items(self):
-        # We added 'about', 'support', 'login' to the list
         return ['home', 'about', 'support', 'login', 'signup']
-
     def location(self, item):
         return reverse(item)
 
 class CourseSitemap(Sitemap):
-    priority = 0.9      # Higher priority because these are your money pages
+    priority = 0.9
     changefreq = 'daily'
-
     def items(self):
-        # This grabs every course from the database automatically
         return Course.objects.all()
-
     def location(self, obj):
-        # This tells Google the exact URL for each course
         return reverse('course_detail', args=[obj.id])
+
+class BlogSitemap(Sitemap):
+    priority = 0.7
+    changefreq = 'weekly'
+    def items(self):
+        return BlogPost.objects.all()
+    def location(self, obj):
+        return reverse('blog_post', args=[obj.slug])
